f7e1ac9f7e8cd07d44e221eef39951a2898f5900
Liuym <liuym@startimes.com.cn>
2019-01-12 10:20:54 +0800

    根据AbstractQueueSynchronizer 实现非重入锁设计

---
+++ src/main/java/lock/NonReentrantLock.java
@@ -0,0 +1,78 @@
+package lock;
+
+import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @author Liuym
+ * @date 2019/1/12 0012
+ */
+public class NonReentrantLock  implements Lock, Serializable{
+    private final Sync sync = new Sync();
+
+    @Override
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public boolean tryLock(long timeout, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1, unit.toNanos(timeout));
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public void unlock() {
+        sync.release(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+
+    static class Sync extends AbstractQueuedSynchronizer{
+
+        @Override
+        protected boolean tryAcquire(int acquires) {
+            assert acquires==1;
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        protected boolean tryRelease(int releases){
+            assert releases==1;
+            if (getState() == 0) {
+                throw new IllegalArgumentException("already unlock");
+            }
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState()==1 && Thread.currentThread()==getExclusiveOwnerThread();
+        }
+
+        protected Condition newCondition(){
+            return new ConditionObject();
+        }
+    }
+}
